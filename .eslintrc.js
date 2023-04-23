0901483bf6535373284d0eb88608b677db226efe
rpjing <rpjing@cn.dten.com>
2023-04-23 19:56:04 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    // "browser": true,
+    "node": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "sourceType": "module"
+  },
+  "rules": {
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "no-console": 0
+  }
+};
